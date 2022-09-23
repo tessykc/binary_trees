199693734dc5037a72c625f1ced2c3e1b4ff5923
tessykc <tessykc.com>
2022-09-22 18:07:35 -0700

    betty error check

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -9,13 +9,12 @@
 binary_tree_t *binary_tree_node(binary_treee_t *parent, int value)
 {
 	*binary_tree_t *new;
-	
 	new = malloc(sizeof(binary_tree_t));
 	if (!new)
-		return(NULL);
+		return (NULL);
 	new->n = value;
 	new->parent = parent;
 	new->left = NULL;
 	new->right = NULL;
-	return(new);
+	return (new);
 }
